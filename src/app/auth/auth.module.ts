58536b0b424b503dac121bfa81c7489e4d5213bb
ngankhanh98 <ngankhanh98@gmail.com>
2020-12-08 16:22:01 +0700

    protected /dashboard

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { NbMenuModule } from "@nebular/theme";
-import { ThemeModule } from "../@theme/theme.module";
-import { AuthComponent } from "./auth.component";
-import { AuthRoutingModule } from "./auth-routing.modules";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NbMenuModule } from '@nebular/theme';
+import { ThemeModule } from '../@theme/theme.module';
+import { AuthRoutingModule } from './auth-routing.modules';
+import { AuthComponent } from './auth.component';
 @NgModule({
   declarations: [AuthComponent],
   imports: [CommonModule, ThemeModule, NbMenuModule, AuthRoutingModule],
---
+++ src/app/auth/state/auth.ts
@@ -0,0 +1,10 @@
+export interface Auth {
+  username: string;
+  token: string;
+  isLoggedIn: boolean;
+}
+
+export interface loginAuth {
+  username: string;
+  password: string;
+}
