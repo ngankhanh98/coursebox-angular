8c3a8b061bd500d04c9d74684f0361cec98a380e
ngankhanh98 <ngankhanh98@gmail.com>
2020-12-08 09:43:19 +0700

    custom route

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,14 +1,14 @@
 import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  { title: "Explore", icon: "globe-3", link: "/explore", home: true },
+  { title: "Explore", icon: "globe-3", link: "explore", home: true },
   {
     title: "Attend",
     icon: "book",
     children: [
-      { title: "Enrolled courses", link: "/enrolled-courses" },
-      { title: "My courses", link: "/my-courses" },
+      { title: "Enrolled courses", link: "enrolled-courses" },
+      { title: "My courses", link: "my-courses" },
     ],
   },
-  { title: "Account", icon: "people", link: "/account" },
+  { title: "Account", icon: "people", link: "account" },
 ];
