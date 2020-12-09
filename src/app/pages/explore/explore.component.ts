162faa2c0b39db5a3f4b0d192a1a7ebf09bbf285
ngankhanh98 <ngankhanh98@gmail.com>
2020-12-09 17:26:37 +0700

    fix: the async way

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Course } from '../state/course';
 import { CourseQuery } from '../state/course.query';
 import { CourseService } from '../state/course.service';
 
@@ -9,9 +8,7 @@ import { CourseService } from '../state/course.service';
   styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
-  courses: Course[] = [];
-
-  // courses$ = this.courseQuery.allBrowseCourses$;
+  courses$ = this.courseQuery.allBrowseCourses$;
 
   constructor(
     private courseService: CourseService,
@@ -20,40 +17,9 @@ export class ExploreComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllCourses();
-    this.courseQuery.allBrowseCourses$.subscribe((arg) => {
-      // console.log('arg', arg);
-      //   this.courses = arg;
-      this.courses = arg;
-      // console.log('this.courses', this.courses);
-    });
   }
 
   getAllCourses() {
     this.courseService.loadCourses();
   }
 }
-
-// without state
-// import { HttpClient } from '@angular/common/http';
-// import { Component, OnInit } from '@angular/core';
-// import { Course } from '../state/course';
-
-// @Component({
-//   selector: 'ngx-explore',
-//   templateUrl: './explore.component.html',
-//   styleUrls: ['./explore.component.scss'],
-// })
-// export class ExploreComponent implements OnInit {
-//   constructor(
-//     private http: HttpClient
-//   ) {}
-//   courses: Course[] = [];
-//   ngOnInit(): void {
-//     this.http
-//       .get<Course[]>('http://localhost:3000/v1/course')
-//       .subscribe((res) => {
-//         console.log('res', res);
-//         this.courses = res;
-//       });
-//   }
-// }
