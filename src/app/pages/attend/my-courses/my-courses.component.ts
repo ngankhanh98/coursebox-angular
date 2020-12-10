057eb667b6a5d00c45f1b45a1bedfc51bf056568
ngankhanh98 <ngankhanh98@gmail.com>
2020-12-10 17:35:48 +0700

    ♻ lift-up state in pages, shared component (card)

--- src/app/pages/attend/my-courses/my-courses.component.ts
+++ src/app/pages/attend/my-courses/my-courses.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CourseQuery } from 'app/pages/state/course.query';
 @Component({
   selector: 'ngx-my-courses',
   templateUrl: './my-courses.component.html',
-  styleUrls: ['./my-courses.component.scss']
+  styleUrls: ['./my-courses.component.scss'],
 })
 export class MyCoursesComponent implements OnInit {
+  // FIXME: fix server (/v1/course retrieve courses with teacher)
+  course$ = this.courseQuery.allBrowseCourses$;
+  constructor(private courseQuery: CourseQuery) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/attend/my-courses/my-courses.module.ts
+++ src/app/pages/attend/my-courses/my-courses.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyCoursesComponent } from './my-courses.component';
 import { ThemeModule } from 'app/@theme/theme.module';
+import { NbButtonModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [MyCoursesComponent],
